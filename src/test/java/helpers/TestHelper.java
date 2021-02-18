5e1beed0b1c969ba2dba644ca471a92be83a07a1
Natalya <n.polyakova.kazanexpress@gmail.com>
2021-02-18 08:28:32 +0300

    added SearchAllureTest, added BING_PAGE to TestHelper, changed JavaCompile to JavaTestCompile in build.gradle

--- src/test/java/helpers/TestHelper.java
+++ src/test/java/helpers/TestHelper.java
@@ -3,4 +3,5 @@ package helpers;
 public class TestHelper {
     public static final String GOOGLE_PAGE = "https://www.google.ru";
     public static final String YANDEX_PAGE = "https://yandex.ru/";
+    public static final String BING_PAGE = "https://www.bing.com/";
 }
